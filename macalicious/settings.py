4ba2c7bcf2c4c8ad793c10e0a155788b7b90e567
Elmer Almeida <elmer@Elmers-Mac-mini.local>
2021-12-09 16:14:26 -0500

    added s3

--- macalicious/settings.py
+++ macalicious/settings.py
@@ -184,3 +184,16 @@ if not DEBUG:
 
 # # Activate Django-Heroku.
 # django_heroku.settings(locals())
+
+# AWS settings
+AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
+AWS_ACCESS_KEY_ID = "AKIA6CLFKBXF6LGUHXVZ"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "printlineinc-bucket"
+AWS_S3_REGION_NAME = 'ca-central-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_ENCRYPTION=True
+AWS_S3_ADDRESSING_STYLE = "virtual"
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
