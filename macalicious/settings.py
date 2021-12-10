5b3a0e8e8f02d6a066cb0f67dcb61d96889a148a
Elmer Almeida <elmer@Elmers-Mac-mini.local>
2021-12-10 18:06:07 -0500

    reset database

--- macalicious/settings.py
+++ macalicious/settings.py
@@ -1,7 +1,6 @@
 import os
 
 import django_heroku
-import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,27 +83,24 @@ WSGI_APPLICATION = 'macalicious.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-# HEROKU: uncomment for deployment
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'd9oaqgr4p1u9f5',
-#             'USER': 'xhjfdfmpcuitpj',
-#             'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
-#             'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
-#             'PORT': '5432',
-#         }
-#     }
-
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd4o9tbvg2edfep',
+            'USER': 'seoogznexzdlxx',
+            'PASSWORD': '001badc5c736f9cf7f6140970d4684ac48ddc49c0d0af936b0fc670c7526de70',
+            'HOST': 'ec2-34-195-69-118.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -172,6 +168,25 @@ LOGIN_REDIRECT_URL = '/shop/'
 # Phone numbers field
 PHONENUMBER_DEFAULT_REGION = 'CA'
 
+if not DEBUG:
+    # Activate Django-Heroku.
+    django_heroku.settings(locals())
+
+# # Activate Django-Heroku.
+# django_heroku.settings(locals())
+
+# AWS settings
+AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
+AWS_ACCESS_KEY_ID = "AKIA6CLFKBXF6LGUHXVZ"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "printlineinc-bucket"
+AWS_S3_REGION_NAME = 'ca-central-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_ENCRYPTION=True
+AWS_S3_ADDRESSING_STYLE = "virtual"
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Email settings
 DEFAULT_FROM_EMAIL = "Macalicious <shop.macalicious@gmail.com>"
@@ -180,42 +195,3 @@ EMAIL_HOST_USER = "shop.macalicious@gmail.com"
 EMAIL_HOST_PASSWORD = "Canada2009!!!"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-# Local sqlite3 database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# HEROKU: uncomment for deployment
-# Heroku database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd9oaqgr4p1u9f5',
-        'USER': 'xhjfdfmpcuitpj',
-        'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
-        'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
-# # Activate Django-Heroku.
-django_heroku.settings(locals())
-
-# AWS settings
-# HEROKU: uncomment for deployment
-AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
-AWS_ACCESS_KEY_ID = "AKIA6CLFKBXF6LGUHXVZ"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "printlineinc-bucket"
-AWS_S3_REGION_NAME = 'ca-central-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_ENCRYPTION = True
-AWS_S3_ADDRESSING_STYLE = "virtual"
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
