3474f6c039047f30b0cbfc7add741540f5ca6015
Elmer Almeida <elmer@Elmers-Mac-mini.local>
2021-12-10 17:57:06 -0500

    used github backup settings.py

--- macalicious/settings.py
+++ macalicious/settings.py
@@ -14,19 +14,20 @@ SECRET_KEY = 'django-insecure-)4l*^n-uu2f&5y^8qdkxo5wp9t4vlwd-b*t4j-1sh8+-y1@tq5
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*'
+    "*"
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'whitenoise.runserver_nostatic',
 
     # Third-party apps
     "crispy_forms",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,8 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'macalicious.urls'
 
@@ -83,25 +84,27 @@ WSGI_APPLICATION = 'macalicious.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 # HEROKU: uncomment for deployment
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'd9oaqgr4p1u9f5',
-            'USER': 'xhjfdfmpcuitpj',
-            'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
-            'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'd9oaqgr4p1u9f5',
+#             'USER': 'xhjfdfmpcuitpj',
+#             'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
+#             'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
+#             'PORT': '5432',
+#         }
+#     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,9 +128,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 # Default primary key field type
@@ -165,6 +172,7 @@ LOGIN_REDIRECT_URL = '/shop/'
 # Phone numbers field
 PHONENUMBER_DEFAULT_REGION = 'CA'
 
+
 # Email settings
 DEFAULT_FROM_EMAIL = "Macalicious <shop.macalicious@gmail.com>"
 EMAIL_HOST = "smtp.gmail.com"
@@ -173,10 +181,6 @@ EMAIL_HOST_PASSWORD = "Canada2009!!!"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# Google Settings (reCaptcha)
-GOOGLE_RECAPTCHA_SITE_KEY = '6LeJ8JIdAAAAAK2_J_1hWKxe19d34fUGkykJuEh8'
-GOOGLE_RECAPTCHA_SECRET_KEY = '6LeJ8JIdAAAAAAphAm0MmXKaq7LSsYe7u7odT7Iw'
-
 # Local sqlite3 database
 # DATABASES = {
 #     'default': {
@@ -184,20 +188,22 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6LeJ8JIdAAAAAAphAm0MmXKaq7LSsYe7u7odT7Iw'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd9oaqgr4p1u9f5',
-#         'USER': 'xhjfdfmpcuitpj',
-#         'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
-#         'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-# Activate Django-Heroku.
+
+# HEROKU: uncomment for deployment
+# Heroku database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9oaqgr4p1u9f5',
+        'USER': 'xhjfdfmpcuitpj',
+        'PASSWORD': '910c838cdd4d647658f93d74b028af93069968153f750732675f3e8a913f74c0',
+        'HOST': 'ec2-34-193-235-32.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+# # Activate Django-Heroku.
 django_heroku.settings(locals())
 
 # AWS settings
