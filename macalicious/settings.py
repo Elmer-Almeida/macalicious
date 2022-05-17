f5d8500775eb2a6a5d4b0ad0a089b9092d8b14b2
Elmer Almeida <almeielm@sheridancollege.ca>
2022-05-17 10:07:28 -0400

    added favicon

--- macalicious/settings.py
+++ macalicious/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)4l*^n-uu2f&5y^8qdkxo5wp9t4vlwd-b*t4j-1sh8+-y1@tq5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Initialize env variables
 env = environ.Env()
